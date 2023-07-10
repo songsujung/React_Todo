b6bf8dd1528c1cb13d29b256466aadb2ad71937e
sj <sujung033131@gmail.com>
2023-07-10 23:58:20 +0900

    0710 querydsl Todo React

--- src/api/TodoAPI.js
+++ src/api/TodoAPI.js
@@ -1,32 +1,37 @@
 import axios from "axios"
 
 // async : 비동기함수로 선언(작업이 완료될 때까지 대기할 수 있는 기능 제공), 항상 Promise의 결과값 반환
-//await : Promise를 처리하고, 결과 반환
+// await : Promise를 처리하고, 결과 반환
 // async와 await는 세트
+// 목록
 export const getList = async() => {
     const res = await axios.get(`http://localhost:8080/api/todos/list`)
 
     return res.data
 } 
 
+// 등록
 export const postTodo = async(todo) => {
     const res = await axios.post(`http://localhost:8080/api/todos/`,todo)
 
     return res.data
 }
 
+// 조회
 export const getTodo = async(tno) => {
     const res = await axios.get(`http://localhost:8080/api/todos/${tno}`)
     
     return res.data
 }
 
+// 삭제
 export const removeTodo = async(tno) => {
     const res = await axios.delete(`http://localhost:8080/api/todos/${tno}`)
     
     return res.data
 }
 
+// 수정
 export const putTodo = async (todo) => {
     const res = await axios.put(`http://localhost:8080/api/todos/${todo.tno}`, todo)
 
--- src/components/todo2/Todo2Input.js
+++ src/components/todo2/Todo2Input.js
@@ -1,19 +1,22 @@
 import { useState } from "react";
 import { postTodo } from "../../api/TodoAPI";
 
+// 기본값 설정
 const initState = {
     title : ''
 }
 
 const Todo2Input = ({changeView}) => {
 
-    const [todo, setTodo] = useState(initState)
+    const [todo, setTodo] = useState(initState) // 상태(오류 안나게끔 기본값 설정)
 
     const handlechangeTodo = (e) => {
         todo[e.target.name] = e.target.value
         setTodo({...todo})
-    }
+    } // Todo를 변경하는 함수 생성
 
+
+    // handleChangeTodo에 파라미터 값이 없어서 onChange 바로 사용가능
     return ( 
         <div className="w-full h-[100vh] bg-blue-800">
             <div className="text-3xl">Todo2Input</div>
